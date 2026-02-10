660fc18795915fefae039a42dd6b4e817a5f1740
Peter Zhang <peter@0g.ai>
2026-02-10 10:16:41 +0800

    update to latest version and fix the test cases

--- src/cmd/indexer.rs
+++ src/cmd/indexer.rs
@@ -134,10 +134,7 @@ pub async fn run_indexer(args: &IndexerArgs) -> Result<()> {
     );
 
     // Start server
-    log::info!(
-        "Starting server at 127.0.0.1:{}",
-        args.endpoint
-    );
+    log::info!("Starting server at 127.0.0.1:{}", args.endpoint);
     let server = HttpServerBuilder::default()
         .build(format!("127.0.0.1:{}", args.endpoint))
         .await
